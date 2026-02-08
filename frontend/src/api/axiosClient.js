4978840ba29d0980bfc00769b7740ceae3d810ff
Jashruth K A <220223967+jashhruthh@users.noreply.github.com>
2026-02-08 17:50:29 +0530

    Update axiosClient.js

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,19 +1,18 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: {
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("exp_token");
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("exp_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
